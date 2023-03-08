ebe541771ff02c5158b23ac95b42980bb41beb63
Gabo <gabocanseco@gmail.com>
2023-03-08 13:12:42 -0600

    Assets agregado

--- src/componets/navbar/Navbar.jsx
+++ src/componets/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
+
 
 const Navbar = () =>{
     return (
-        <div>
-            Navbar
+        <div className="gtp_navbar">
+            <div className="gtp_navbar_links">
+                <div className="gpt_navbar_links_logo">
+                    <img src={logo} alt="Logo"/>
+                </div>
+            </div>
         </div>
     )
 };
