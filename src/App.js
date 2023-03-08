a0a9fe592104e6b539fa02e3b8b4c13ad036cce6
Gabo <gabocanseco@gmail.com>
2023-03-07 21:20:08 -0600

    Styles básico, containers y components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import { Footer, Blog, Possibility, Features, WhatGPT, Header} from './containers/index'
+import { CTA, Brand, Navbar } from './componets';
+import './app.css';
 const App = () =>{
     return (
-        <div>
-            <h1>
-                GPT-Chat
-            </h1>
+        <div className="App">
+            <div className="gradient">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 };
---
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './whatgpt.css';
+
+const WhatGPT = () =>{
+    return (
+        <div>
+            WhatGPT
+        </div>
+    )
+};
+
+export default WhatGPT;
